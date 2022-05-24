bbfc4c2a5f053fbc5a5484240b01aa0d3029a6f5
Bipin Manandhar <kubipin07@gmail.com>
2022-05-24 13:29:48 +0545

    Add livedown package for live preview of markdown files.

--- packages.el
+++ packages.el
@@ -11,6 +11,7 @@
 (package! string-inflection)
 (package! dap-mode)
 (package! js-doc)
+(package! livedown)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
